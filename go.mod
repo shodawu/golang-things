f7bd8274d28a514e2286117519796c21e66eaac9
rick.wuu <rick.wuu@atrustek.com>
2021-02-22 11:52:27 +0800

    FTP controller

--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.14
 require (
 	github.com/cucumber/godog v0.11.0
 	github.com/cucumber/messages-go/v10 v10.0.3
+	github.com/jlaffaye/ftp v0.0.0-20201112195030-9aae4d151126
+	github.com/tealeg/xlsx/v3 v3.2.3
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
--- svcnotify/svcnotify.go
+++ svcnotify/svcnotify.go
@@ -2,8 +2,15 @@ package svcnotify
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
+	"time"
+
+	"github.com/jlaffaye/ftp"
+	_ "github.com/tealeg/xlsx/v3"
+	_ "gopkg.in/gomail.v2"
 )
 
 // FTPConfig ...
@@ -31,7 +38,8 @@ type Config struct {
 
 // SvcNotify ...
 type SvcNotify struct {
-	Config Config
+	Config  Config
+	FileBuf []byte
 }
 
 // LoadConfig 讀取設定
@@ -40,5 +48,34 @@ func (svc *SvcNotify) LoadConfig(filePath string) {
 	b, _ := ioutil.ReadAll(file)
 
 	json.Unmarshal(b, &svc.Config)
+
+}
+
+// FTPController ...
+func (svc *SvcNotify) FTPController() {
+	c, err := ftp.Dial(fmt.Sprintf("%v:%v", svc.Config.FTP.Host, svc.Config.FTP.Port), ftp.DialWithTimeout(5*time.Second))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = c.Login(svc.Config.FTP.Account, svc.Config.FTP.Pwd)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r, err := c.Retr(svc.Config.FTP.DocPath)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Close()
+
+	svc.FileBuf, err = ioutil.ReadAll(r)
+}
+
+// TriggerExec ...
+func TriggerExec() {
+	svc := SvcNotify{}
+	svc.LoadConfig("config.json")
+	svc.FTPController()
 
 }
